8c6ddd4ac3eeeb70d394e16ea842120dacf78c95
Adit Khokar <adit@iitk.ac.in>
2020-05-14 19:06:05 +0530

    ac

--- assignments/aiml/bot0.py
+++ assignments/aiml/bot0.py
@@ -1,26 +1,35 @@
 import aiml
 import os
+from autocorrect import Speller
+
+spell = Speller()
 
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 
-if os.path.isfile("bot_brain.brn"):
+if os.path.isfile("bot_brain.brn") and retrain_model == "N":
     kernel.bootstrap(brainFile = "bot_brain.brn")
     print("Using old brain")
 else:
-    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "LOAD AIML B")
     kernel.saveBrain("bot_brain.brn")
     print("Using new brain")
 
 
 # Press CTRL-C to break this loop
 while True:
-    message = input("Enter your message to the bot: ")
+    message = input("you > ")
     if message == "quit":
         exit()
     elif message == "save":
         kernel.saveBrain("bot_brain.brn")
     else:
-        bot_response = kernel.respond(message)
-        print(bot_response)
-        # Do something with bot_response
+        query = message
+        query = [spell(w) for w in (query.split())]
+        question = " ".join(query)
+        response = kernel.respond(question)
+        if response:
+            print("bot > ", response)
+        else:
+            print("bot > :) ", )
+        
